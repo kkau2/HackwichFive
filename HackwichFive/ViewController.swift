23e01e88f31ba2538fdcc817ba7fb9e8a99ec520
kkau2 <46541270+kkau2@users.noreply.github.com>
2019-03-07 10:34:54 -1000

    Completed Part 1 & 2 of Hackwich #5

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var slider: UISlider!
+    
+    //Create a variable that can hold the value of the slider
+    var currentValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,10 +25,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sliderHasMoved(_ sender: Any) {
+    
+        print("The value of the slider is:\(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
+    
     @IBAction func myGuessButtonPressed(_ sender: Any) {
     
+    //5. New variable message that displays a message that includes currentValue
+        let message = "The value is: \(currentValue)"
+        
     //1. Create alert view
-        let alert = UIAlertController(title:"Hello World!", message: "This is my first alert view", preferredStyle: .alert)
+        let alert = UIAlertController(title:"Hello World!", message: message, preferredStyle: .alert)
         
     //2. button that user taps to dismiss view controller
         let action =  UIAlertAction(title: "Awesome", style: .default, handler: nil)
