1e3b52444029b62b8507794cfab2ee0e251eb82b
kkau2 <46541270+kkau2@users.noreply.github.com>
2019-03-14 10:22:14 -1000

    Working

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -12,12 +12,26 @@ class ViewController: UIViewController {
 
     @IBOutlet var slider: UISlider!
     
+    @IBOutlet var targetLabel: UILabel!
+    
     //Create a variable that can hold the value of the slider
     var currentValue: Int = 0
+    
+    //Create a variable to hold the target value
+    var targetValue: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        currentValue = lroundf(slider.value)
+        
+        //Set targetValue to a randomly generated number that the acr4random_uniform function
+        //Removed after typing line 44: targetValue = 1 + Int(arc4random_uniform(100))
+        
+        //call startNewRound
+        startNewRound()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,11 +45,27 @@ class ViewController: UIViewController {
         print("The value of the slider is:\(slider.value)")
         
     }
+        
+        func startNewRound() {
+            
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+            
+        updateTargetLabel()
+            
+        }
+    
+    func updateTargetLabel() {
+        
+        targetLabel.text = String(targetValue)
+        
+    }
     
     @IBAction func myGuessButtonPressed(_ sender: Any) {
     
     //5. New variable message that displays a message that includes currentValue
-        let message = "The value is: \(currentValue)"
+        let message = "The value is: \(currentValue)" + "\nThe target value is: \(targetValue)"
         
     //1. Create alert view
         let alert = UIAlertController(title:"Hello World!", message: message, preferredStyle: .alert)
@@ -48,6 +78,8 @@ class ViewController: UIViewController {
         
     //4. Present alertview on the screen
         present(alert, animated: true, completion: nil)
+        
+    startNewRound()
         
   }
     
